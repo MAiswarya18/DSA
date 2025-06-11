026763a885ede88f09cc4636fad28bbc789ba0c3
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-11 11:17:37 +0530

    day4 completed

---
+++ Day4 task/Day4Task1.java
@@ -0,0 +1,65 @@
+package addon;
+
+public class Day4Task1 {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int val) {
+            data = val;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public Day4Task1() {
+        head = null;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    public static void main(String[] args) {
+        Day4Task1 list = new Day4Task1();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtBeginning(5);
+        list.insertAtBeginning(1);
+        list.display();
+    }
+}
+ 
